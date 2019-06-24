4526fc931e110b75eccda04ef852d2d2e7c83256
Mateusz Wykurz <wykurz@gmail.com>
2019-06-24 01:14:51 -0400

    Cleanup old C++ typedefs; get rid of type conversion warning - I want my for loops with ints

--- cpp-config/template.cpp
+++ cpp-config/template.cpp
@@ -26,9 +26,14 @@
 
 using namespace std;
 
-using uint = unsigned int;
-using ll = long long;
-using ull = unsigned long long;
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
 using sstrm = stringstream;
 using ld = long double;
 
